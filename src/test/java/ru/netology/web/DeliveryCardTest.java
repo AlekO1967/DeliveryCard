93f808b51bd143bdda45494bb1e4ca1d1f96ee05
Aleksandr Osipov <eltisprim@mail.ru>
2021-05-22 23:24:07 -0400

    Added some testes

--- src/test/java/ru/netology/web/DeliveryCardTest.java
+++ src/test/java/ru/netology/web/DeliveryCardTest.java
@@ -22,4 +22,29 @@ public class DeliveryCardTest {
         $(byText("Забронировать")).click();
         $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
     }
+
+    @Test
+    void shouldEnterNotValidCity() {
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Wendell");
+        $("[data-test-id='name'] input").setValue("Александр Сергеевич Осипов");
+        $("[data-test-id='phone'] input").setValue("+79801234567");
+        $("[data-test-id=agreement]").click();
+        $(byText("Забронировать")).click();
+        //$(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".input_invalid[data-test-id='city']").shouldHave(text("Доставка в выбранный город недоступна"));
+    }
+
+    @Test
+    void shouldSendRequestForCityFromList() {
+        open("http://localhost:9999");
+        $("[data-test-id=city] input").setValue("Ворон");
+        $$(".menu-item__control").last().click();
+        $("[data-test-id=name] input").setValue("Александр Сергеевич Осипов");
+        $("[data-test-id='phone'] input").setValue("+79801234567");
+        $("[data-test-id=agreement]").click();
+        $(byText("Забронировать")).click();
+        $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+    }
+
 }
